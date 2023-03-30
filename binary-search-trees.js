8135647d79bcd7108546c0ab1c845a424c317920
Carlos Andrés <ca.castaneda@outlook.com>
2023-03-29 23:07:35 -0500

    Add prettyPrint function to visualize our tree

--- binary-search-trees.js
+++ binary-search-trees.js
@@ -29,3 +29,17 @@ const buildTree = (array) => {
     }
     return root;
 };
+
+//This function will expect to receive the root of your tree as the value for the node parameter.
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+    if (node === null) {
+        return;
+    }
+    if (node.right !== null) {
+        prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.val}`);
+    if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+};
